f289a1a02d658ca9b38452a1bc5f03620a71245a
Diego <diegofti123@gmail.com>
2024-04-19 19:32:20 -0400

    integracion modal ver carrito

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,23 @@
+
 const carrito = []
 
+$('#boton-modal').on('click', function () {
+  const mimodal = document.querySelector('#modal-contenido')
+  //NECESITO VER COMO EVITAR LOS INNERHTML
+  carrito.forEach(element => {
+    mimodal.innerHTML += `<img src="/assets/img/${element}.png" style="width:200px"></img>`
+  });
+})
 
-function AgregarQuitar (producto){
+
+function AgregarQuitar(producto) {
   const enCarrito = carrito.find((e) => e === producto.attributes[0].value)
   if (!enCarrito) {
     carrito.push(producto.attributes[0].value)
-    $(producto).css( "opacity", "0.5" )
+    $(producto).css("opacity", "0.5")
   } else {
     carrito.splice(carrito.indexOf(producto.attributes[0].value), 1)
-    $(producto).css( "opacity", "1" )
+    $(producto).css("opacity", "1")
   }
   console.log(carrito)
 }
